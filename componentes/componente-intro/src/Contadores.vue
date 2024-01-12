6d4f4084671f5e8f4baf66b3b7aea8e8c5992979
IgorHenrik1 <igorhenrikg1@gmail.com>
2024-01-12 15:01:15 -0300

    Modulo 8

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-for="c in 5" :key="c" />
+    </div>
+</template>
+
+<script>
+import contador from './Contador.vue';
+export default {
+    components: { 'app-contador': contador },
+};
+</script>
+
+<style></style>
